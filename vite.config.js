9b024f784f7722c5c3bd467919131287879f2834
Bijan Amir Hojat <b.amirhojat@iwink.nl>
2026-02-21 22:12:12 +0100

    added autoupdate

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'app_icon.jpeg'],
+      manifest: {
+        name: 'Divide/Chores',
+        short_name: 'Divide/Chores',
+        description: 'Takenlijst app voor het huishouden',
+        start_url: '/divide-chores/',
+        display: 'standalone',
+        background_color: '#FEFDFB',
+        theme_color: '#A7F3D0',
+        icons: [
+          {
+            src: '/app_icon.jpeg',
+            sizes: '512x512',
+            type: 'image/jpeg',
+            purpose: 'any'
+          },
+          {
+            src: '/app_icon.jpeg',
+            sizes: '512x512',
+            type: 'image/jpeg',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpeg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+            }
+          }
+        ]
+      }
+    })
+  ],
   base: '/divide-chores/'
 })
